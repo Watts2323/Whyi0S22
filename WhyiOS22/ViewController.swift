442790521b50695fc68b69235ccdf0120d233246
Xavier <xwatts2323@xaviers-mbp.lan>
2018-10-17 20:55:55 -0600

    Initial commit

---
+++ WhyiOS22/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhyiOS22
+//
+//  Created by Xavier on 10/17/18.
+//  Copyright © 2018 Xavier ios dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
